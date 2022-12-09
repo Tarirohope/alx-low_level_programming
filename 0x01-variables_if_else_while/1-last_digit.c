00e25c308a84d47e3c2d26a3eb4a3d6c0d060d1d
tarirohope <ptkpriscila@gmail.com>
2022-12-09 15:09:04 -0800

    last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
- /**
-  * main - main block
-  * Description: Print statements based on the last digit of the random number.
-  * Return: 0
-  */
+/**
+ * main - Entry point
+ *
+ * Description: Print statements based on the last digit of the random number.
+ *
+ * Return: 0
+ */
 
 int main(void)
 
