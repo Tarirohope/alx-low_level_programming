24e220be9ec6b5f32c763d5e630cbe31986e9372
tarirohope <ptkpriscila@gmail.com>
2023-01-04 15:04:00 -0800

    sum of diagsonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagsums - print the number of two
+ * diagsonals of a square matrix of integers
+ * @a: input pointers.
+ * @size: size of the matrix.
+ * Return: no return.
+ */
+voidprint_diagsum(int *a, int size)
+{
+	inti, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
