5a11d0265c35769965c6a4db318ca757c392b32c
tarirohope <ptkpriscila@gmail.com>
2023-01-09 08:27:06 -0800

    print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion - print string in reverse using recursion
+ * _print_rev_recursion - print string in revers.
  * @s: string
  * Return: no return.
  */
@@ -11,6 +11,5 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 	}
-	else
-		_putchar('\0');
+
 }
